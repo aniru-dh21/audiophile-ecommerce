fc182d7353ee2327be20ea2b668c4c24af0a92ef
Ramachandra Anirudh Vemulapalli <anirudh@Ramachandras-MacBook-Air.local>
2024-07-11 19:13:24 +0530

    List Component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Banner from "@/components/home/Banner";
+import NavList from "@/components/NavList";
 
 export default function Home() {
   return (
     <main className="">
       <Banner></Banner>
+      <NavList />
     </main>
   );
 }
